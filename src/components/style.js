8a5546ebb948d8ee9b4b2414b555d8ec38812806
jkidune <kidunejoseph91@gmail.com>
2020-07-21 20:53:34 +0300

    commit on 'master'

---
+++ src/components/style.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Style() {
+    return(
+        <div>
+            
+           <Link to="/" >Click me</Link>
+           
+        </div>
+    );
+}
+
+export default Style;
