b033b692bd70a2407da27fe9b2b7a8bbc2cd128b
jkidune <kidunejoseph91@gmail.com>
2020-07-23 21:43:16 +0300

    Updates

---
+++ precache-manifest.6553c4b63b98f6287a218e98d5b667fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c30beaf0e296b431579a75dad50c81",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "98221bb8d93adb0eaa72",
+    "url": "/todoapp/static/css/main.e5e75c83.chunk.css"
+  },
+  {
+    "revision": "1adba280f785b65c5ede",
+    "url": "/todoapp/static/js/2.36b1da36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.36b1da36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98221bb8d93adb0eaa72",
+    "url": "/todoapp/static/js/main.8204eebc.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
